a408458fe986aacb629191e636130c87c644f2a1
yinhang_sun <sunyinhang@gmail.com>
2023-01-19 22:32:01 -0800

    Creating first version of List Todos Page

---
+++ src/main/java/com/yinhang/mypersonalwebapp/todo/TodoController.java
@@ -0,0 +1,33 @@
+package com.yinhang.mypersonalwebapp.todo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TodoController {
+	
+	private TodoService todoService;
+	
+	
+	public TodoController(TodoService todoService) {
+		super();
+		this.todoService = todoService;
+	}
+
+
+
+	// /list-todos
+	@RequestMapping("list-todos")
+	public String listAllTodos(ModelMap model) {
+		
+		List<Todo> todos = todoService.findByUsername("Yinhang");
+		model.addAttribute("todos", todos);
+		
+		return "listTodos";
+	}
+
+}
