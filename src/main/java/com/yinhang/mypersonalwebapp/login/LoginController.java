201f7dc38c72bcb58b8aca08a7adcdeccbd06481
yinhang_sun <sunyinhang@gmail.com>
2023-01-19 18:47:55 -0800

    Displaying Login Credentials in a JSP using Model

--- src/main/java/com/yinhang/mypersonalwebapp/login/LoginController.java
+++ src/main/java/com/yinhang/mypersonalwebapp/login/LoginController.java
@@ -3,21 +3,25 @@ package com.yinhang.mypersonalwebapp.login;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class LoginController {
 
-	
-	// login => com.yinhang.mypersonalwebapp.login.LoginController => login.jsp
-	
-	//http://localhost:8080/login?name=yinhang
-	//Model
-	@RequestMapping("login")
-	public String gotoLoginPage(@RequestParam String name, ModelMap model) {
-		model.put("name", name);
-		
-		System.out.println("Request param is " + name); //
+	@RequestMapping(value="login", method=RequestMethod.GET)
+	public String gotoLoginPage() {		
 		return "login";
+	}
+	
+	@RequestMapping(value="login", method=RequestMethod.POST)
+	//login?name=Yinhang RequestParam
+	public String gotoWelcomePage(@RequestParam String name, 
+			@RequestParam String password, ModelMap model) {	
+		model.put("name", name);
+		model.put("password", password);
+		
+		return "welcome";
 	}
 }
