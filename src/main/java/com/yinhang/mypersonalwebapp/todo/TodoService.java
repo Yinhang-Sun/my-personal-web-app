0b5d5c2d2ddad3899af8f863f4d24535d4ae0ede
yinhang_sun <sunyinhang@gmail.com>
2023-01-20 17:16:11 -0800

    Implement Update Todo : Show Update Todo Page and Save changes to Todo

--- src/main/java/com/yinhang/mypersonalwebapp/todo/TodoService.java
+++ src/main/java/com/yinhang/mypersonalwebapp/todo/TodoService.java
@@ -7,6 +7,8 @@ import java.util.function.Predicate;
 
 import org.springframework.stereotype.Service;
 
+import jakarta.validation.Valid;
+
 @Service
 public class TodoService {
 	
@@ -15,7 +17,7 @@ public class TodoService {
 	private static int todosCount = 0; 
 	
 	static {
-		todos.add(new Todo(++todosCount, "Yinhang", "Learn AWS", 
+		todos.add(new Todo(++todosCount, "Yinhang", "Get AWS Certified", 
 				LocalDate.now().plusYears(1), false));
 		todos.add(new Todo(++todosCount, "Yinhang", "Learn DevOps", 
 				LocalDate.now().plusYears(2), false));
@@ -36,9 +38,20 @@ public class TodoService {
 		
 		// todo.getId() == id;		
 		// lambda function: todo -> todo.getId == id
-		Predicate<? super Todo> predicate 
-			= todo -> todo.getId() == id;
+		Predicate<? super Todo> predicate = todo -> todo.getId() == id;
 		todos.removeIf(predicate);
+	}
+
+	public Todo findById(int id) {
+		Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+		Todo todo = todos.stream().filter(predicate).findFirst().get();
+		return todo;
+	}
+
+	public void updateTodo(@Valid Todo todo) {
+		deleteById(todo.getId());
+		todos.add(todo);
+		
 	}
 	
 
